ce2bc55f394ee9675296efc4515588a2cf99a7c9
Selvalakshmi <selvalakshmivkm@gmail.com>
2023-10-11 16:19:11 +0400

    restrucring Project and introduced UseEffect hook to tell engine to excute particular function/code on load (Only once)

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,7 +1,10 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+console.log("Hello");
 
+useEffect(()=>{
   const reqBody = {
     username:"selva",
     password:"selva"
@@ -12,7 +15,15 @@ function App() {
     },
     method:"post",
     body:JSON.stringify(reqBody),
-  });
+  }).then((response)=> { return Promise.all([response.json(),response.headers]
+    )})
+    .then(([body,headers]) => {
+      console.log("Header :  "+headers.get("authorization"));
+      console.log(body);
+    });
+
+},[]);
+  
   return (
     <div className="App">
      <h1> Hello World!!!</h1>
